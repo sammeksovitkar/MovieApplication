4a8f7ac19adf8457c70c52bd305e741244b16eb3
sammeksovitkar <sammeksovitkar1998@gmail.com>
2024-03-11 14:10:16 +0530

    fix errors

--- src/pages/home/heroBanner/HeroBanner.jsx
+++ src/pages/home/heroBanner/HeroBanner.jsx
@@ -4,7 +4,7 @@ import { useNavigate } from "react-router-dom";
 import useFetch from "../../../hooks/useFetch";
 import { useSelector } from "react-redux";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
-import Img from "../../../components/lazyLoadImage/Img";
+// import Img from "../../../components/lazyLoadImage/Img";
 
 const HeroBanner = () => {
   const [background, setBackground] = useState("");
@@ -31,7 +31,8 @@ const HeroBanner = () => {
     <div className="heroBanner">
       {!loading && (
         <div className="backdrop-img">
-          <Img src={background} />
+          {/** <Img src={background} /> */}
+          Hero banner
         </div>
       )}
       <div className="opacity-layer"></div>
